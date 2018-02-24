12e24942c2482c7401b69a858d0929759b2e48ad
yash gupta <yash@akeo.no>
2018-02-24 17:03:49 +0530

    tokenDistributor contract created

--- contracts/CycledToken.sol
+++ contracts/CycledToken.sol
@@ -1,95 +1,23 @@
 pragma solidity ^0.4.18;
 
-import "../node_modules/zeppelin-solidity/contracts/token/StandardToken.sol";
-import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../node_modules/zeppelin-solidity/contracts/token/PausableToken.sol";
+import "../node_modules/zeppelin-solidity/contracts/token/BurnableToken.sol";
 
-contract CycledToken is StandardToken, Ownable {
+/**
+ * @title Cycled Token
+ */
+contract CycledToken is PausableToken, BurnableToken {
     string public constant name = "CycledToken";
     string public constant symbol = "CYD";
     uint8 public constant decimals = 18;
 
-
     /// Maximum tokens to be allocated.
-    uint256 public constant HARD_CAP = 100000000 * 10**uint256(decimals);
-
-    /// no tokens can be ever issued when this is set to "true"
-    bool public tokenSaleClosed = false;
-
-    /// investors can directly invest and get the token when this is set to "true"
-    bool public tradingOpen = false;
-
-    /// Issue event index starting from 0.
-    uint64 public issueIndex = 0;
-
-    /// Emitted for each sucuessful token purchase.
-    event Issue(uint64 issueIndex, address addr, uint256 tokenAmount);
-
-    //// Modifiers start
-    modifier inProgress {
-        require(totalSupply < HARD_CAP && !tokenSaleClosed);
-        _;
-    }
-
-    /// Allow the closing to happen only once
-    modifier beforeEnd {
-        require(!tokenSaleClosed);
-        _;
-    }
-
-    //// Modifiers end
+    uint256 public constant HARD_CAP = 1000000000 * 10**uint256(decimals);
     
     function CycledToken() public {
+        totalSupply = HARD_CAP;
+        balances[msg.sender] = totalSupply;
+        Transfer(0x0, msg.sender, totalSupply);
     }
 
-     /// @dev enable the token trading
-    function enableTrading() public onlyOwner {
-        tradingOpen = true;
-    }
-  
-    /// @dev disable the token trading
-    function disableTrading() public onlyOwner {
-        tradingOpen = false;
-    }
-
-    /// @dev Issue tokens for a single buyer on the presale
-    /// @param _beneficiary addresses that the presale tokens will be sent to.
-    /// @param _tokens the amount of tokens, with decimals expanded (full).
-    function issueTokens(address _beneficiary, uint256 _tokens) public onlyOwner inProgress {
-        require(_beneficiary != address(0));
-
-        // compute without actually increasing it
-        uint256 increasedTotalSupply = totalSupply.add(_tokens);
-        // roll back if hard cap reached
-        require(increasedTotalSupply <= HARD_CAP);
-
-        // increase token total supply
-        totalSupply = increasedTotalSupply;
-        
-        // update the beneficiary balance to number of tokens sent
-        balances[_beneficiary] = balances[_beneficiary].add(_tokens);
-        
-        // event for token transfer
-        Transfer(msg.sender, _beneficiary, _tokens);
-        // event is fired when tokens issued
-        Issue(issueIndex++, _beneficiary, _tokens);
-    }
-    
-    /// @dev Closes the sale, 
-    function close() public onlyOwner beforeEnd {
-        /// no more tokens can be issued after this line
-        tokenSaleClosed = true;
-        
-    }
-
-    /// Transfer limited by the tradingOpen modifier 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(tradingOpen);
-        return super.transferFrom(_from, _to, _value);
-    }
-
-    /// Transfer limited by the tradingOpen modifier 
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(tradingOpen);
-        return super.transfer(_to, _value);
-    }
 }
---
+++ contracts/TokenDistributor.sol
@@ -0,0 +1,109 @@
+pragma solidity ^0.4.18;
+
+import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
+import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./CTestToken.sol";
+
+contract TokenDistributor is Ownable {
+    using SafeMath for uint256;
+
+    // The token being sold
+    CTestToken private token;
+
+    // USe to set the base rate
+    uint256 private baseRate;
+
+    // Total Token Sold
+    uint256 public tokenSold;
+
+    // Total Wei raised
+    uint256 public weiRaised = 0;
+
+    uint8 private constant DECIMAL = 18;
+
+    /// no tokens can be ever issued when this is set to "true"
+    bool public tokenSaleClosed = false;
+
+    /// Issue event index starting from 0.
+    uint64 public issueIndex = 0;
+
+    /// Maximum tokens to be allocated on the PreSale (20% of the hard cap)
+    uint256 public constant PRESALE_SUPPLY = 200000000 * 10**uint256(DECIMAL);
+
+    /// Emitted for each sucuessful token purchase.
+    event Issue(uint64 issueIndex, address addr, uint256 tokenAmount);
+
+    /// Pre-Sale start Date
+    uint64 private constant date15Mar2018 = 1519381303;
+
+    /// Pre-Sale end Date
+    uint64 private constant date12Apr2018 = 1525219199;
+
+    modifier inProgress {
+        require(tokenSold <= PRESALE_SUPPLY && !tokenSaleClosed);
+        _;
+    }
+
+    /// Allow the closing to happen only once
+    modifier beforeEnd {
+        require(!tokenSaleClosed);
+        _;
+    }
+
+    /**
+    * @param _rate Number of token units a buyer gets per wei
+    * @param _tokenAddress of the token being sold
+    */
+    function TokenDistributor(uint256 _rate, address _tokenAddress) public {
+        require(_rate > 0);
+        require(_tokenAddress != address(0));
+
+        baseRate = _rate;
+        token = CTestToken(_tokenAddress);
+    }
+
+    /// @dev Issue tokens for a single buyer on the presale
+    /// @param _beneficiary addresses that the presale tokens will be sent to.
+    /// @param _investedWieAmount the amount to invest, with decimals expanded (full).
+    function issueTokens(address _beneficiary, uint256 _investedWieAmount) public onlyOwner inProgress {
+        require(_beneficiary != address(0));
+        require(_investedWieAmount != 0);
+
+        //Compute number of tokens to transfer
+        uint256 tokens = getTokenAmount(_investedWieAmount);
+
+        // compute without actually increasing it
+        uint256 increasedtokenSold = tokenSold.add(tokens);
+
+        // roll back if hard cap reached
+        require(increasedtokenSold <= PRESALE_SUPPLY);
+
+        // increase token total supply
+        tokenSold = increasedtokenSold;
+        //increase wie raised
+        weiRaised = weiRaised.add(_investedWieAmount);
+
+        //Transfering tokens from issue token wallet to beneficiary wallet
+        token.transferFrom(owner, _beneficiary, tokens);
+
+        // event is fired when tokens issued
+        Issue(issueIndex++, _beneficiary, tokens);
+    }
+    
+
+    function getTokenAmount(uint256 _weiAmount) internal view returns (uint256 tokens) {
+        uint256 _tokenSold = tokenSold * 10**uint256(DECIMAL);
+        uint256 _tokenBase = _weiAmount.mul(baseRate);
+        uint256 _amountFor30perDiscount = 75000000 * 10**uint256(DECIMAL);
+
+        if (_tokenSold >= _amountFor30perDiscount)
+            tokens = _tokenBase.mul(30).div(100).add(_tokenBase);
+        else
+            tokens = _tokenBase.mul(50).div(100).add(_tokenBase);
+    }
+
+    function close() public onlyOwner beforeEnd {
+        tokenSaleClosed = true;
+    }
+
+}
